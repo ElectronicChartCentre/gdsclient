5311ea734cb8beec05345866811719006e5d8a0b
Kjetil <kjetil@ecc.no>
2022-06-15 09:48:31 +0200

    need to allow cookies to be able to download

--- c#/ws.imp/CommonClient.cs
+++ c#/ws.imp/CommonClient.cs
@@ -36,15 +36,17 @@ namespace gdsclientc.ws.imp
 
 		private static System.ServiceModel.Channels.Binding GetBinding()
 		{
-			System.ServiceModel.BasicHttpsBinding binding = new System.ServiceModel.BasicHttpsBinding();
-			if (binding == null)
-				return null;
-			binding.Security.Mode = System.ServiceModel.BasicHttpsSecurityMode.Transport;
-			binding.Security.Transport.ClientCredentialType = System.ServiceModel.HttpClientCredentialType.Basic;
-			binding.Security.Transport.ProxyCredentialType = System.ServiceModel.HttpProxyCredentialType.None;
-			binding.Security.Message.ClientCredentialType = System.ServiceModel.BasicHttpMessageCredentialType.UserName;
-			binding.MaxReceivedMessageSize = 2147483647;
-			return binding;
+			var httpsBinding = new BasicHttpsBinding();
+			httpsBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+			httpsBinding.Security.Mode = BasicHttpsSecurityMode.Transport;
+
+			var integerMaxValue = int.MaxValue;
+			httpsBinding.MaxBufferSize = integerMaxValue;
+			httpsBinding.MaxReceivedMessageSize = integerMaxValue;
+			httpsBinding.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+			httpsBinding.AllowCookies = true;
+
+			return httpsBinding;
 		}
 	}
 }
